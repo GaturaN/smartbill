972189f4752bffcde5b87e4e2a0c96f8d4b5fb85
GaturaN <gaturanjenga@gmail.com>
2024-09-01 10:40:11 +0300

    feat: bill_functions

--- smartbill/smartbill/doctype/bill/bill.js
+++ smartbill/smartbill/doctype/bill/bill.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2024, Gatura Njenga and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Bill", {
-// 	refresh(frm) {
+frappe.ui.form.on("Bill", {
+	// refresh(frm) {
 
-// 	},
-// });
+	// },
+    validate(frm){
+        // amount should not be less than 100
+        if (frm.doc.amount < 100) {
+            frappe.throw ("Amount should not be less than 100");
+        }
+    },
+
+    type(frm){
+        // if type is recurring, show recurring model
+        frm.toggle_display(["recurring_model"], frm.doc.type === "Recurring Payment");
+        frm.toggle_reqd(["recurring_model"], frm.doc.type === "Recurring Payment");
+    }
+});
--- smartbill/smartbill/doctype/bill/bill.py
+++ smartbill/smartbill/doctype/bill/bill.py
@@ -1,9 +1,39 @@
 # Copyright (c) 2024, Gatura Njenga and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe.utils import getdate
 from frappe.model.document import Document
 
 
 class Bill(Document):
-	pass
+	def validate(self):
+		validate_type(self)
+		validate_amount(self)
+		validate_due_date(self)
+  		# pass
+
+	
+def validate_type(self):
+    # make recurring_model mandatory if bill type is recurring
+    if self.type == "Recurring Payment":
+        if not self.recurring_model:
+            frappe.throw("Recurring model is required")
+            
+            
+def validate_amount(self):
+    # amount should not be less than 100
+    amount = self.amount
+    if amount:
+        if amount < 100:
+            frappe.throw("Bill amount should not be less than 100")
+            
+def validate_due_date(self):
+    # due_date should not be in the past and should be over a week from now
+	due_date = getdate(self.due_date)
+	if due_date:
+		today = getdate()
+		if due_date < today:
+			frappe.throw("Due date cannot be in the past")
+		elif (due_date - today).days < 7:
+			frappe.throw("Due date should be at least 7 days from now")
